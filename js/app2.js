d5dbc6d6eeee48d855a2f3f321142bb3e4020a59
ModjoInc <habibem@gmail.com>
2017-07-17 13:32:29 +0300

    final

---
+++ js/app2.js
@@ -0,0 +1,43 @@
+//callback
+
+/*let handleClick = function(event) {
+  alert("Hello");
+}
+
+let bouton = document.querySelector('h2');
+bouton.addEventListener('click', handleClick);
+console.dir(handleClick);
+//time
+let int = setInterval(function() {
+  myTimer()
+}, 1000);
+
+function myTimer() {
+  let d = new Date();
+  let t = d.toLocaleTimeString();
+  document.getElementById('demo').innerHTML = t;
+}
+
+function stopFunction() {
+  clearInterval(int);
+}
+*/
+
+function disapear(event) {
+    let selElem = event.target;
+    selElem.style['display'] = 'none';
+}
+
+function appear(event) {
+    let selElem = event.target;
+    selElem.style['display'] = 'block';
+  }
+
+  let divD = document.querySelectorAll('.container');
+for (var i = 0; i < divD.length; i++) {
+  divD[i].//suite:::::::::::::::::::::
+}
+
+
+//
+console.dir(divD);
